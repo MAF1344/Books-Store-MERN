f06cdf971bad46d8b12124307dcd00a774895471
MAF1344 <muha22126ti@student.nurulfikri.ac.id>
2025-02-04 18:44:25 +0700

    first commit, documentation

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root:root@books-store-mern.jr8sb.mongodb.net/?retryWrites=true&w=majority&appName=Books-Store-MERN';
+
+// database for yourself
