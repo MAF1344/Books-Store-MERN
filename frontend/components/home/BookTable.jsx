9d194bcd7b636a8078ef917e7c3c976eb9445504
MAF1344 <muha22126ti@student.nurulfikri.ac.id>
2025-02-06 18:09:37 +0700

    create book modal component

--- frontend/components/home/BookTable.jsx
+++ frontend/components/home/BookTable.jsx
@@ -3,7 +3,7 @@ import {AiOutlineEdit} from 'react-icons/ai';
 import {BsInfoCircle} from 'react-icons/bs';
 import {MdOutlineAddBox, MdOutlineDelete} from 'react-icons/md';
 
-const BookTable = ({books}) => {
+const BooksTable = ({books}) => {
   return (
     <table className="w-full border-separate border-spacing-2">
       <thead>
@@ -20,8 +20,8 @@ const BookTable = ({books}) => {
           <tr key={book._id} className="h-8">
             <td className="border border-slate-700 rounded-md text-center">{index + 1}</td>
             <td className="border border-slate-700 rounded-md text-center">{book.title}</td>
-            <td className="border border-slate-700 rounded-md text-center">{book.author}</td>
-            <td className="border border-slate-700 rounded-md text-center">{book.publishYear}</td>
+            <td className="border border-slate-700 rounded-md text-center max-md:hidden">{book.author}</td>
+            <td className="border border-slate-700 rounded-md text-center max-md:hidden">{book.publishYear}</td>
             <td className="border border-slate-700 rounded-md text-center">
               <div className="flex justify-center gap-x-4">
                 <Link to={`/books/details/${book._id}`}>
@@ -42,4 +42,4 @@ const BookTable = ({books}) => {
   );
 };
 
-export default BookTable;
+export default BooksTable;
